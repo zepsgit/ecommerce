d18144933db2f4d6429398946ba445c5ac17cd6b
zepeng <zepengchen423@gmail.com>
2021-11-30 20:43:09 -0330

    add the header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react'
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
-
+import Header from './components/header/header.component';
 function App() {
   return (
     <div className="app">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,6 @@ import './menu-item.css'
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
-    
-    console.log(match);
     return (
         <div className={`menu-item ${size}`}
             onClick={() => history.push(`${match.url}${linkUrl}`)}
@@ -12,7 +10,7 @@ const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
                 style={{ backgroundImage: `url(${imageUrl})` }}
             />
             <div className="content">
-                <h1 className="title">{title.toUpperCase()}</h1>
+                <h1 className="title">{title.toLowerCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
